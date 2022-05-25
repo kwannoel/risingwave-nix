1a10a0ad646bf1fa75533a44550c2275055d7dfe
Noel Kwan <noelkwan1998@gmail.com>
2022-05-25 20:38:51 +0800

    cleanup

--- shell.nix
+++ shell.nix
@@ -3,12 +3,26 @@
 
 let unstable = import (fetchTarball https://nixos.org/channels/nixos-unstable/nixexprs.tar.xz) {};
     baseInputs = with pkgs; [
-    protobuf pkg-config openssl
-    rustup cacert curl which cmake
+    # std
+    pkg-config openssl
+
+    # deps
+    protobuf
+    cacert curl cmake
     git tmux ncurses postgresql
-    less cargo-sweep
-    cargo-make
+
+    # build
     lld
+    which
+    less
+
+    # rust
+    rustup
+    cargo-make
+    cargo-sweep
+
+    # tools
+    ripgrep
   ];
 
 in
@@ -19,15 +33,27 @@ pkgs.mkShell {
     a() { ./risedev d; psql -h localhost -p 4566; }
     k() { ./risedev k; }
     rl() { ./risedev l; }
-    cr() { cargo sweep --toolchains="nightly"; }
     cl() { ./risedev clean-data; }
     fr() { ./risedev run-planner-test; }
     fs() { ./risedev apply-planner-test; }
     fa() { ./risedev do-apply-planner-test; }
     e2e() { ./risedev d; \
             ./risedev slt -p 4566 './e2e_test/streaming/**/*.slt'; \
+            ./risedev slt -p 4566 './e2e_test/batch/**/*.slt'; \
             ./risedev k; \
             ./risedev clean-data; \
           }
+    e2es() { ./risedev d; \
+            ./risedev slt -p 4566 './e2e_test/streaming/**/*.slt'; \
+            ./risedev k; \
+            ./risedev clean-data; \
+          }
+    e2eb() { ./risedev d; \
+            ./risedev slt -p 4566 './e2e_test/batch/**/*.slt'; \
+            ./risedev k; \
+            ./risedev clean-data; \
+          }
+    w() { cargo watch; }
+    cr() { cargo sweep --toolchains="nightly"; }
   '';
 }
