29988f15628934c472ca75d58ffc0cf59c938fc7
Noel Kwan <noelkwan1998@gmail.com>
2022-06-08 18:52:28 +0800

    specify dev database

--- shell.nix
+++ shell.nix
@@ -35,7 +35,7 @@ pkgs.mkShell {
   shellHook = with pkgs; ''
     export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${openssl.out}/lib:${curl.out}/lib
     cd ~/projects/risingwave
-    a() { ./risedev d; psql -h localhost -p 4566; }
+    a() { ./risedev d; psql -h localhost -p 4566 -d dev; }
     k() { ./risedev k; }
     rl() { ./risedev l; }
     cl() { ./risedev clean-data; }
@@ -43,18 +43,18 @@ pkgs.mkShell {
     fs() { ./risedev apply-planner-test; }
     fa() { ./risedev do-apply-planner-test; }
     e2e() { ./risedev d; \
-            ./risedev slt -p 4566 './e2e_test/streaming/**/*.slt'; \
-            ./risedev slt -p 4566 './e2e_test/batch/**/*.slt'; \
+            ./risedev slt -p 4566 -d dev './e2e_test/streaming/**/*.slt'; \
+            ./risedev slt -p 4566 -d dev './e2e_test/batch/**/*.slt'; \
             ./risedev k; \
             ./risedev clean-data; \
           }
     e2es() { ./risedev d; \
-            ./risedev slt -p 4566 './e2e_test/streaming/**/*.slt'; \
+            ./risedev slt -p 4566 -d dev './e2e_test/streaming/**/*.slt'; \
             ./risedev k; \
             ./risedev clean-data; \
           }
     e2eb() { ./risedev d; \
-            ./risedev slt -p 4566 './e2e_test/batch/**/*.slt'; \
+            ./risedev slt -p 4566 -d dev './e2e_test/batch/**/*.slt'; \
             ./risedev k; \
             ./risedev clean-data; \
           }
