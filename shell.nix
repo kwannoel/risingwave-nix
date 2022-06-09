dcd48aa7b4839dec3598a13bb5ea912b16984b49
Noel Kwan <noelkwan1998@gmail.com>
2022-06-09 14:50:22 +0800

    patch user flags

--- shell.nix
+++ shell.nix
@@ -43,18 +43,18 @@ pkgs.mkShell {
     fs() { ./risedev apply-planner-test; }
     fa() { ./risedev do-apply-planner-test; }
     e2e() { ./risedev d; \
-            ./risedev slt -p 4566 -d dev -U root './e2e_test/streaming/**/*.slt'; \
-            ./risedev slt -p 4566 -d dev -U root './e2e_test/batch/**/*.slt'; \
+            ./risedev slt -p 4566 -d dev -u root './e2e_test/streaming/**/*.slt'; \
+            ./risedev slt -p 4566 -d dev -u root './e2e_test/batch/**/*.slt'; \
             ./risedev k; \
             ./risedev clean-data; \
           }
     e2es() { ./risedev d; \
-            ./risedev slt -p 4566 -d dev -U root './e2e_test/streaming/**/*.slt'; \
+            ./risedev slt -p 4566 -d dev -u root './e2e_test/streaming/**/*.slt'; \
             ./risedev k; \
             ./risedev clean-data; \
           }
     e2eb() { ./risedev d; \
-            ./risedev slt -p 4566 -d dev -U root './e2e_test/batch/**/*.slt'; \
+            ./risedev slt -p 4566 -d dev -u root './e2e_test/batch/**/*.slt'; \
             ./risedev k; \
             ./risedev clean-data; \
           }
