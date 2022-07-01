6d9bd4c8850e7cd30566138c6a305fa9df24344e
Noel Kwan <noelkwan1998@gmail.com>
2022-07-01 20:04:22 +0800

    add fmt, clippy + update cmds

--- shell.nix
+++ shell.nix
@@ -10,7 +10,7 @@ let unstable = import (fetchTarball https://nixos.org/channels/nixos-unstable/ni
     # deps
     protobuf
     cacert curl cmake
-    git tmux ncurses postgresql
+    git tmux ncurses postgresql_14
 
     # build
     lld
@@ -24,6 +24,8 @@ let unstable = import (fetchTarball https://nixos.org/channels/nixos-unstable/ni
 
     # tools
     ripgrep
+    gdb
+    ps
   ];
 
 in
@@ -35,31 +37,40 @@ pkgs.mkShell {
   shellHook = with pkgs; ''
     export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${openssl.out}/lib:${curl.out}/lib
     cd ~/projects/risingwave
-    a() { ./risedev d; psql -h localhost -p 4566 -d dev -U root; }
+    a() { ./risedev d; \
+          psql -h localhost -p 4566 -d dev -U root;
+        }
     k() { ./risedev k; }
     rl() { ./risedev l; }
     cl() { ./risedev clean-data; }
     fr() { ./risedev run-planner-test; }
     fs() { ./risedev apply-planner-test; }
     fa() { ./risedev do-apply-planner-test; }
+    e2ebasic() { ./risedev d; \
+            ./risedev slt -p 4566 -d dev -u root './e2e_test/streaming/basic.slt'; \
+            ./risedev k; \
+            ./risedev clean-data; \
+          }
     e2e() { ./risedev d; \
-            ./risedev slt -p 4566 -d dev -u root './e2e_test/streaming/**/*.slt'; \
-            ./risedev slt -p 4566 -d dev -u root './e2e_test/batch/**/*.slt'; \
+            ./risedev slt -p 4566 -d dev -u root -j 1 './e2e_test/streaming/**/*.slt'; \
+            ./risedev slt -p 4566 -d dev -u root -j 1 './e2e_test/batch/**/*.slt'; \
             ./risedev k; \
             ./risedev clean-data; \
           }
     e2es() { ./risedev d; \
-            ./risedev slt -p 4566 -d dev -u root './e2e_test/streaming/**/*.slt'; \
+            ./risedev slt -p 4566 -d dev -u root -j 1 './e2e_test/streaming/**/*.slt'; \
             ./risedev k; \
             ./risedev clean-data; \
           }
     e2eb() { ./risedev d; \
-            ./risedev slt -p 4566 -d dev -u root './e2e_test/batch/**/*.slt'; \
+            ./risedev slt -p 4566 -d dev -u root -j 1 './e2e_test/batch/**/*.slt'; \
             ./risedev k; \
             ./risedev clean-data; \
           }
     w() { cargo watch; }
     wa() { cargo watch -x 'check --all-targets'; }
     cr() { cargo sweep --toolchains="nightly"; }
+    fmt() { ./risedev c; }
+    clippy() { cargo clippy --workspace --all-targets --fix --allow-dirty; }
   '';
 }
